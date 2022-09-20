14e0b05fbc5c41e02fef7bc406a18f23650bf4fa
wanqingfu <wanqingfu@microsoft.com>
2022-09-20 08:43:18 +0800

    fix: add random to disk_resource_id

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -26,7 +26,11 @@ echo "SIG_GALLERY_NAME is ${SIG_GALLERY_NAME}"
 echo "SIG_GALLERY_NAME_FROM_JSON is ${SIG_GALLERY_NAME_FROM_JSON}"
 
 sig_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/${SIG_GALLERY_NAME}/images/${SIG_IMAGE_NAME}/versions/${GEN2_CAPTURED_SIG_VERSION}"
-disk_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/disks/${GEN2_CAPTURED_SIG_VERSION}"
+# Adding ${RANDOM} as suffix because Windows Gen 2 is reusing sigMode. The $GEN2_CAPTURED_SIG_VERSION comes from $SIG_IMAGE_VERSION, which may
+# be the same "1.0.0" for cpncurrent production pipelines. If one pipeline fails, the disk resource will not be deleted successfully, leading to the following error
+# "(PropertyChangeNotAllowed) Changing property 'imageReference' is not allowed." This is because for later pipeline runs, the $disk_resource_id is the same, but the image
+# reference $sig_resource_id is different.
+disk_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/disks/${GEN2_CAPTURED_SIG_VERSION}${RANDOM}"
 
 echo "sig_resource_id is ${sig_resource_id}"
 echo "disk_resource_id is ${disk_resource_id}"
